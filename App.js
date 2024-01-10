4f34f8c7d3c374065f51b50e7c58eb2e04ccc96d
quoctri2002 <155059577+quoctri2002@users.noreply.github.com>
2024-01-10 14:07:22 +0700

    quoctri2

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { Dashboard } from './Screens/Home';
+import { Login } from './Screens/Auth';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Detail from './Screens/Discover/Detail';
 
 export default function App() {
   return (
