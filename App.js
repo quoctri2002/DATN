1e6c2ae6fa07958f54fddb6faf12c9bce8bb636e
Minhld-21 <100395104+Minhld-21@users.noreply.github.com>
2024-04-12 14:00:50 +0700

    yah

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import { Provider, useSelector } from 'react-redux';
 import store from './store';
 import { SafeAreaView } from 'react-native';
 import { AppNavigation } from './Navigations';
-import {HomeScreen} from './Screens/Home/index';
 
 export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaView style={{ flex: 1 }}>
-        <HomeScreen />
+        <AppNavigation />
       </SafeAreaView>
     </Provider>
   );
